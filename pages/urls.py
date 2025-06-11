8497bce516286665223d34595da16b4d343a275c
Prekshya <bisuraskoti@gmail.com>
2025-06-11 07:29:02 +0545

    templates fix

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from pages import views
 
+app_name = 'pages'
+
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about_view, name='about'),
     path('services/', views.services_view, name='services'),
     path('portfolio/', views.portfolio_view, name='portfolio'),
+    path('team/', views.team_view, name='team'), 
     path('contact/', views.contact_view, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/home.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'pages/about.html')
 
 def services_view(request):
-    return render(request, 'service-details.html')
+    return render(request, 'pages/services.html')
 
 def portfolio_view(request):
-    return render(request, 'portfolio-details.html')
+    return render(request, 'pages/portfolio.html')
+
+def team_view(request):
+    return render(request, 'pages/team.html')  
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'pages/contact.html')
